3c2d57a7b1b17da55b3dc91b67a5c27aa974014d
Rupesh Kumar <rupesh.kumar.light@gmail.com>
2025-10-15 22:51:22 +0530

    update

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -8,9 +8,14 @@ const About = () => {
                             <img src="../aboutmain.png" alt="about main" />
                         </div>
                         <div className="col-md-6">
-                            <h2>Why Choose Us</h2>
-                            <h3>IT Solutions Designed to Drive Results</h3>
-                            <p className="">Monotonectally synergize granular markets and front market Collaboratively visualize strat fomediaries after based models, Synergistically task state of the art infrastructures.</p>
+                            <h2 className="main_title">About Your Company</h2>
+                            <h3>We Execute Ideas From Start to Finish</h3>
+                            <p>Monotonectally synergize granular markets and front market Collaboratively visualize strat fomediaries after based models, Synergistically task state of the art infrastructures.</p>
+                            <ul>
+                                <li><i class="fa-solid fa-circle-check"></i>Created 40+ unique sections with responsiveness.</li>
+                                <li><i class="fa-solid fa-circle-check"></i>You will able to build a new site with an ease.</li>
+                                <li><i class="fa-solid fa-circle-check"></i>Booster is made for stay ahead from the compitition.</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import './style/slider.css'
 import App from './App.jsx'
+import './style/pages_style.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
